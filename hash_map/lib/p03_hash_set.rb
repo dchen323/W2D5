18fd1a8c2fb7c0eb2d8032d4ca95f18eca261077
Daniel Chen <d.chen323@gmail.com>
2018-04-27 14:59:54 -0700

    Complete hash_set

--- hash_map/lib/p03_hash_set.rb
+++ hash_map/lib/p03_hash_set.rb
@@ -9,17 +9,33 @@ class HashSet
   end
 
   def insert(key)
+    resize!  if @count == num_buckets
+    unless include?(key)
+      @store[self[key]] << key
+      @count +=1
+    end
   end
 
   def include?(key)
+    @store[self[key]].each do |bucket|
+      return true if bucket == key
+    end
+    false
   end
 
   def remove(key)
+    @store[self[key]].each do |bucket_el|
+      if bucket_el == key
+        @store[self[key]] -= [bucket_el]
+        @count -=1
+      end
+    end
   end
 
   private
 
   def [](num)
+    num.hash % num_buckets
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -28,5 +44,14 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.each do |bucket|
+      bucket.each do |el|
+        insert(el)
+      end
+    end
+      @store
   end
 end
